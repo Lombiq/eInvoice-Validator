aec5de6d444b633fb5135276a1059ef6c0a13bd7
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-05-26 13:31:53 +0200

    Add tostring override

--- Lombiq.EInvoiceValidator/Models/FailedAssert.cs
+++ Lombiq.EInvoiceValidator/Models/FailedAssert.cs
@@ -1,4 +1,11 @@
 ﻿#nullable enable
 namespace Lombiq.EInvoiceValidator.Models;
 
-public record FailedAssert(string? Id, string? Location, string? Test, bool IsError, string? Text);
+public record FailedAssert(string? Id, string? Location, string? Test, bool IsError, string? Text)
+{
+    public override string ToString() =>
+        $"{(IsError ? "Error" : "Warning")}: {Text} \n" +
+        $"ID: {Id}) \n" +
+        $"Location: {Location}) \n" +
+        $"Test: {Test}";
+}

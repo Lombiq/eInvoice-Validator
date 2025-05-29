316651e58966a1dbcf03fbef65bf0aae91583a1e
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-05-29 12:05:53 +0200

    Add docs

--- Lombiq.EInvoiceValidator/Services/ISchematronValidationService.cs
+++ Lombiq.EInvoiceValidator/Services/ISchematronValidationService.cs
@@ -4,8 +4,15 @@ using System.Threading.Tasks;
 
 namespace Lombiq.EInvoiceValidator.Services;
 
+/// <summary>
+/// Provides a service for executing schematron validation on XML strings for eInvoices.
+/// </summary>
 public interface ISchematronValidationService
 {
+    /// <summary>
+    /// Executes schematron validation on the provided XML string for the specified invoice format. Calls a Node.js process to perform the validation
+    /// with SaxonJs.
+    /// </summary>
     Task<SchematronValidationResult> ExecuteSchematronValidationAsync(
         string xml,
         InvoiceFormat format,

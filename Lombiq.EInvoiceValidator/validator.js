fb12d0a859100a783a058d3dd021e884b13733a0
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-05-30 11:21:27 +0200

    Little fixes

--- Lombiq.EInvoiceValidator/validator.js
+++ Lombiq.EInvoiceValidator/validator.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved -- ESLint does not know where to find external modules. */
 const saxonJs = require('saxon-js');
 const fs = require('fs');
 
@@ -17,25 +18,24 @@ module.exports = {
     validateAsText: async (convertedSchematronFilePath, xmlTextToValidate) => {
         try {
             const start = Date.now();
-            debugger;
             const stylesheet = getOrLoadStylesheet(convertedSchematronFilePath);
 
             const result = await saxonJs.transform({
                 stylesheetInternal: stylesheet,
                 sourceText: xmlTextToValidate,
                 sourceType: 'xml',
-                destination: 'serialized'
+                destination: 'serialized',
             }, 'async');
 
             const duration = Date.now() - start;
 
             return {
                 outputXml: result.principalResult,
-                durationMs: duration
+                durationMs: duration,
             };
         }
         catch (error) {
             return 'An unexpected fatal error happened: ' + error;
         }
-    }
+    },
 };
